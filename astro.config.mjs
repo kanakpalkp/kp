8298217f2a75da56334925ac7408a5f4dc617c03
kanakpalkp <kanakpalpanwar26@gmail.com>
2025-12-18 19:52:03 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import edgeoneAdapter from '@edgeone/astro';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: edgeoneAdapter(),
+  output: 'static',
+  integrations: [react()]
+});
